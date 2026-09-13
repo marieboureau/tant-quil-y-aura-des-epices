441e0b44eb32a8dc97822655111dce2eaaf8b26f
marieboureau <marieboureau@gmail.com>
2026-09-13 18:40:46 +0200

    Sprint 5 - fichiers pwa

---
+++ public/sw.js
@@ -0,0 +1,56 @@
+const CACHE = 'epices-app-v1'
+const APP_SHELL = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/icon.svg'
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', event => {
+  const request = event.request
+  const url = new URL(request.url)
+
+  if (request.method !== 'GET') return
+  if (url.origin !== self.location.origin) return
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then(response => {
+          const clone = response.clone()
+          caches.open(CACHE).then(cache => cache.put('/index.html', clone))
+          return response
+        })
+        .catch(() => caches.match('/index.html'))
+    )
+    return
+  }
+
+  event.respondWith(
+    caches.match(request).then(cached => {
+      const network = fetch(request).then(response => {
+        if (response && response.ok) {
+          const clone = response.clone()
+          caches.open(CACHE).then(cache => cache.put(request, clone))
+        }
+        return response
+      }).catch(() => cached)
+
+      return cached || network
+    })
+  )
+})
